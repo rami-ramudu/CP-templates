b6bf62a40e3c872fcdc5d5ed92065d5c5066f0c7
rami-ramudu <89202765+rami-ramudu@users.noreply.github.com>
2021-12-05 23:02:14 +0530

    Create Tiles4XNspace1X4tile.cpp

---
+++ Tiles4XNspace1X4tile.cpp
@@ -0,0 +1,101 @@
+/*
+     CP template
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+#define int            long long int
+#define F              first
+#define S              second
+#define b              begin
+#define e              end
+#define pb             push_back
+#define si             set <int>
+#define vi             vector <int>
+#define pii            pair <int, int>
+#define vpi            vector <pii>
+#define vpp            vector <pair<int, pii>>
+#define mii            map <int, int>
+#define mpi            map <pii, int>
+#define umi            unordered_map<int,int>
+#define spi            set <pii>
+#define endl           "\n"
+#define sz(x)          ((int) x.size())
+#define all(p)         p.begin(), p.end()
+#define double         long double
+#define que_max        priority_queue <int>
+#define que_min        priority_queue <int, vi, greater<int>>
+#define bug(...)       __f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)       for(auto x : a) cout << x << " "; cout << endl
+#define print1(a)      for(auto x : a) cout << x.F << " " << x.S << endl
+#define print2(a,x,y)  for(int i = x; i < y; i++) cout<< a[i]<< " "; cout << endl
+ 
+inline int power(int a, int b)
+{
+	int x = 1;
+	while (b)
+	{
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+
+void __f (const char* names, Arg1&& arg1, Args&&... args)
+{
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+int tiles(int n)
+{
+	if(n<4) return 1;
+	return tiles(n-1)+tiles(n-4);
+}
+void solve() {
+  
+  
+   int a,n;
+   cin>>n;
+   cout<<"The number of ways to place the tiles is: "<<tiles(n)<<endl;
+   
+   
+   
+     
+
+
+}
+
+
+
+
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt",  "r",  stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	clock_t z = clock();
+
+	int t = 1;
+	 cin >> t;
+	while (t--) solve();
+
+	cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC);
+
+	return 0;
+}
